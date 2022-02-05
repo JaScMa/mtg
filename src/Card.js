3c56c0fa4c6afd5dd0054531ced0b0c42eba5da5
JaScMa <63072931+JaScMa@users.noreply.github.com>
2022-02-05 21:49:08 +0100

    hover

--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,24 @@
 import defaultPicture from "./img/card-background.jpeg"
 import "./style/card.css"
+import { useState } from "react";
 
 const Card = ({ card }) => {
     const img = card.imageUrl || defaultPicture ;
+    const [hovered, setHovered] = useState(false);
     console.log(card.imageUrl);
     console.log(img);
 
    
 
     return (
-        <div className="card">
+        <div className="card"
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave ={() => setHovered(false)}
+        >
             <div className="card-picture">
                 <img className="card-img" src={img} alt="artwork"></img>
-                { (img === defaultPicture) &&
-                    <div className="test">
+                { (img === defaultPicture || hovered) &&
+                    <div className="overlay-text">
                         <h1>{card.name}</h1>
                         <p>{card.text}</p>
                     </div> 
