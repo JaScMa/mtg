165e4e4b968cff3e437b742c7d86b99837769920
JaScMa <63072931+JaScMa@users.noreply.github.com>
2022-02-09 00:13:38 +0100

    gradient

--- src/Result.js
+++ src/Result.js
@@ -3,13 +3,16 @@ import Card from "./Card";
 
 const Result = ({ cards }) => {
     return (
-         (!cards.length) ? 
+        <div className="grid gap-3 grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+
+        { (!cards.length) ? 
             <h1>Sorry, we couldn't find any matching cards</h1> :
             (
                 cards.map(card => (
                     <Card card={card} key={card.id} />
                 ))
-            )
+            )}
+        </div>
     )
 }
 
