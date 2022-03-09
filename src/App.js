a5681714d9b889c78fb0b6d0736abf10d17b5b7a
JaScMa <63072931+JaScMa@users.noreply.github.com>
2022-03-09 15:20:07 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Search from "./Search";
 import Details from "./Details";
-import { HashRouter, Route, Link, Switch } from "react-router-dom";
+import { HashRouter, Route, Link, Routes } from "react-router-dom";
 import { ReactComponent as Home } from "./img/home.svg";
 
 const App = () => {
@@ -8,13 +8,13 @@ const App = () => {
 		<HashRouter>
 			<div className="h-screen w-screen fixed overflow-y-scroll bg-gradient-to-t from-blueishGreen via-lightPurple to-darkPurple">
 				<header>
-					<Link to="/"><Home className="m-2" alt="home" width="20pt" height="20pt" fill="white"/></Link>
+					<Link to="/mtg/"><Home className="m-2" alt="home" width="20pt" height="20pt" fill="white"/></Link>
 				</header>
 				<main>
-					<Switch>
-						<Route path="/Details/:id" element={<Details />} />
-						<Route path="/" element={<Search />} />
-					</Switch>
+					<Routes>
+						<Route path="/mtg/Details/:id" element={<Details />} />
+						<Route path="/mtg/" element={<Search />} />
+					</Routes>
 					<div className="h-20 w-screen"></div>
 				</main>
 			</div>
