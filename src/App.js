cc81908428c4b7927208da61f99f7db2763d9b0a
JaScMa <63072931+JaScMa@users.noreply.github.com>
2022-03-09 12:26:58 +0100

    HashRouter2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Search from "./Search";
 import Details from "./Details";
-import {BrowserRouter, Route, Link, Routes, HashRouter} from "react-router-dom";
+import {BrowserRouter, Route, Link, Routes} from "react-router-dom";
 import { ReactComponent as Home } from "./img/home.svg";
 
 const App = () => {
   return (
-		<HashRouter>
+		<BrowserRouter>
 			<div className="h-screen w-screen fixed overflow-y-scroll bg-gradient-to-t from-blueishGreen via-lightPurple to-darkPurple">
 				<header>
 					<Link to="/"><Home className="m-2" alt="home" width="20pt" height="20pt" fill="white"/></Link>
@@ -18,7 +18,7 @@ const App = () => {
 					<div className="h-20 w-screen"></div>
 				</main>
 			</div>
-		</HashRouter>
+		</BrowserRouter>
 	);
 }
 
