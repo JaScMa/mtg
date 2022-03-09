100f65aeed222a319319936115ed83cc2150c03d
JaScMa <63072931+JaScMa@users.noreply.github.com>
2022-03-09 15:24:35 +0100

    link without mtg

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ const App = () => {
 		<HashRouter>
 			<div className="h-screen w-screen fixed overflow-y-scroll bg-gradient-to-t from-blueishGreen via-lightPurple to-darkPurple">
 				<header>
-					<Link to="/mtg/"><Home className="m-2" alt="home" width="20pt" height="20pt" fill="white"/></Link>
+					<Link to="/"><Home className="m-2" alt="home" width="20pt" height="20pt" fill="white"/></Link>
 				</header>
 				<main>
 					<Routes>
-						<Route path="/mtg/Details/:id" element={<Details />} />
-						<Route path="/mtg/" element={<Search />} />
+						<Route path="/Details/:id" element={<Details />} />
+						<Route path="/" element={<Search />} />
 					</Routes>
 					<div className="h-20 w-screen"></div>
 				</main>
